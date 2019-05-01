227699dc49b981e9024d8b79ae33e40a435762f6
Erwan Lagrange <lagrange.erwan@outlook.com>
2019-05-02 00:21:22 +0200

    ajout caneva custom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-window.addEventListener('scroll', () => {
-  let parent =  document.getElementById('parallax-container');
-  let children = parent.getElementsByTagName("div");
-  for(let i = 0; i < children.length; i++) {
-    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
-  }
-}, false)
+$(document).ready(function(){
+    // au clic sur un lien
+    $('a').on('click', function(evt){
+       // bloquer le comportement par défaut: on ne rechargera pas la page
+       evt.preventDefault(); 
+       // enregistre la valeur de l'attribut  href dans la variable target
+ var target = $(this).attr('href');
+       /* le sélecteur $(html, body) permet de corriger un bug sur chrome 
+       et safari (webkit) */
+ $('html, body')
+       // on arrête toutes les animations en cours 
+       .stop()
+       /* on fait maintenant l'animation vers le haut (scrollTop) vers 
+        notre ancre target */
+       .animate({scrollTop: $(target).offset().top}, 1000 );
+    });
+});
